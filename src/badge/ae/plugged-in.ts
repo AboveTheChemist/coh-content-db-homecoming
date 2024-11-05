031a59f2f48eb21267ae892c6ba85dd83901bdf5
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2024-11-05 10:43:41 -0500

    Update and rename architect-x.ts to plugged-in.ts
    updated badge info for name change

--- src/badge/ae/plugged-in.ts
+++ src/badge/ae/plugged-in.ts
@@ -1,17 +1,17 @@
 import {ALIGNMENT_ANY, BadgeType, IBadgeData} from "coh-content-db";
 
-export const ArchitectX: IBadgeData = {
+export const PluggedIn: IBadgeData = {
     type: BadgeType.AE,
-    key: "architect-x",
+    key: "plugged-in",
     setTitleId: 1235,
     names: [
-        {value: "Architect X"}
+        {value: "Plugged In"}
     ],
     alignment: ALIGNMENT_ANY,
     badgeText: [{value: "You've earned 10 Mission Architect badges!"}],
     acquisition: "Earn 10 Mission Architect badges to earn this badge.",
     links: [
-        {title: "Architect X Badge", href: "https://homecoming.wiki/wiki/Architect_X_Badge"}
+        {title: "Plugged In Badge", href: "https://homecoming.wiki/wiki/Plugged_In_Badge"}
     ],
-    icons: [{value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/ae/architect-x.png"}]
+    icons: [{value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/ae/plugged-in.png"}]
 };
